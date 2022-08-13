a04f9ec9ea9be898ee289cc568cf44ddffb8a7b9
yjjyzzz <550363988@qq.com>
2022-08-13 15:38:21 +0800

    完成表格

--- api/data.js
+++ api/data.js
@@ -13,3 +13,13 @@ export const getData = () => {
         url: '/home/getData',
     })
 }
+
+export const getUser = (params) => {
+    return axios.request(
+        {
+            url: '/user/getUser',
+            methods: 'get',
+            params
+        }
+    )
+}
